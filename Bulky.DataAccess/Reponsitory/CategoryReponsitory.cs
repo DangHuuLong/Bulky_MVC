1fd122ed023563c2508234f7cfcceaa50278b7c7
Long Đặng Hữu <danghuulong394@gmail.com>
2025-02-28 14:08:21 +0700

    Implement Category Repository

---
+++ Bulky.DataAccess/Reponsitory/CategoryReponsitory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Reponsitory.IReponsitory;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Reponsitory
+{
+    public class CategoryReponsitory : Reponsitory<Category>, ICategoryReponsitory
+    {
+        private ApplicationDbContext _db;
+        public CategoryReponsitory(ApplicationDbContext db):base(db) 
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ Bulky.DataAccess/Reponsitory/IReponsitory/ICategoryReponsitory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Reponsitory.IReponsitory
+{
+    public interface ICategoryReponsitory: IReponsitory<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
