f99825cbdb698e7981ad2e1b507e450d4d389830
Long Đặng Hữu <danghuulong394@gmail.com>
2025-03-01 12:34:24 +0700

    Loading Navigation Properties

--- Bulky.DataAccess/Reponsitory/IReponsitory/IReponsitory.cs
+++ Bulky.DataAccess/Reponsitory/IReponsitory/IReponsitory.cs
@@ -10,8 +10,8 @@ namespace BulkyBook.DataAccess.Reponsitory.IReponsitory
     public interface IReponsitory<T> where T : class
     {
         //T - Category
-        IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filer);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filer, string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
