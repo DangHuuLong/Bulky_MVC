63deac2c6465f4579ead53ce116fd08e12302c37
Long Đặng Hữu <danghuulong394@gmail.com>
2025-03-02 22:50:57 +0700

    Dynamic Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -27,21 +27,21 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
             };
 
-            foreach(var cart  in ShoppingCartVM.ShoppingCartList)
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
-                cart.Price = GetPriceBasedOnQuantity(cart);
-                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+                double price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (price * cart.Count);
             }
 
             return View(ShoppingCartVM);
         }
 
-        private double GetPriceBasedOnQuantity (ShoppingCart shoppingCart)
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
-            if(shoppingCart.Count <= 50)
+            if (shoppingCart.Count <= 50)
             {
                 return shoppingCart.Product.Price;
-            } 
+            }
             else
             {
                 if (shoppingCart.Count <= 100)
