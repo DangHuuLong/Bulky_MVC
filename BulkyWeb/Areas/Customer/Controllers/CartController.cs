9ef8dfba951fc0ada49d0e0bcd8e2c85a0340070
Long Đặng Hữu <danghuulong394@gmail.com>
2025-03-02 22:39:32 +0700

    Get Order Total in Shopping Cart

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
 using BulkyBookBook.DataAccess.Reponsitory.IReponsitory;
 using Microsoft.AspNetCore.Authorization;
@@ -26,7 +27,32 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
             };
 
+            foreach(var cart  in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+            }
+
             return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBasedOnQuantity (ShoppingCart shoppingCart)
+        {
+            if(shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            } 
+            else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
         }
     }
 }
